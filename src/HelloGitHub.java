f4300cfde2cd8072b2860a821f7a4e80c2d11477
vadim novitskyi <thrownovitskyi@gmail.com>
2022-10-07 21:22:18 +0300

    what is that?

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -2,4 +2,7 @@ public class HelloGitHub {
     public static void main(String[] args) {
         System.out.println("hello GitHub");
     }
+    public void print(){
+        System.out.println("second text");
+    }
 }
